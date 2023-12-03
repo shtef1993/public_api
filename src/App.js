649cd2f41ad939fc0e87904f4658ea796806c036
Shtef1993 <stjepan.pentavec10@gmail.com>
2023-12-03 12:03:30 +0100

    chuck_norris_jokes_app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
+const ChuckNorrisJokesApp = () => {
+  const [joke, setJoke] = useState('');
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(true); 
+
+  const fetchJoke = async () => {
+    try {
+      const response = await fetch('https://api.chucknorris.io/jokes/random');
+      const data = await response.json();
+      setJoke(data.value);
+      setIsButtonClicked(true);
+    } catch (error) {
+      console.error('Error fetching Chuck Norris joke:', error);
+    }
+  };
+
+  const resetState = () => {
+    setJoke('');
+    setIsButtonClicked(false);
+  };
+
+  const toggleTheme = () => {
+    setIsDarkTheme((prevTheme) => !prevTheme);
+  };
+
   return (
-    <div className="App">
+    <div className={`App ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Chuck Norris Jokes App</h1>
+        <button className="btn btn-primary" onClick={fetchJoke}>
+          Get Chuck Norris Joke
+        </button>
+        {isButtonClicked && (
+          <div className="mt-4">
+            <p className="lead">{joke}</p>
+            <button className="btn btn-secondary" onClick={resetState}>
+              Reset
+            </button>
+          </div>
+        )}
+        <button className="btn btn-info mt-4" onClick={toggleTheme}>
+          Toggle Theme
+        </button>
       </header>
     </div>
   );
-}
+};
 
-export default App;
+export default ChuckNorrisJokesApp;
